50043b063f5bec24e80cd4f87e144e9c219c245e
Pavel Rukin <rivelr@gmail.com>
2020-07-15 14:00:33 +0300

    add fragments

---
+++ app/src/main/java/com/example/covid19/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.covid19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        val navController = host.navController
+        setupNavigation()
+    }
+
+
+    fun setupNavigation() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            bottomNavigationView,
+            navHostFragment!!.navController
+        )
+    }
+    }
